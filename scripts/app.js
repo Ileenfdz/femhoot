8b4eb4060372c596f19725e1a179dd739b217401
Ileenfdz <ileenfdz@gmail.com>
2022-01-23 23:09:48 +0100

    Added function get the name of the user, and links to all pages

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,10 @@
+import { getName } from './modules/getName.js';
+
+const loginBtn = document.getElementById('loginBtn');
+
+if(loginBtn){
+    loginBtn.addEventListener('click', getName);
+}
 
 export function app() {
     console.log('connected to app');
