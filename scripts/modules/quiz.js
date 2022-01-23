c14508bbf0dab1f09748443313c3e811d4e05196
Sara <sara.gonzalezsa@gmail.com>
2022-01-23 16:59:13 +0100

    created quiz.js and array quiz

---
+++ scripts/modules/quiz.js
@@ -0,0 +1,14 @@
+const quiz = new Array();
+
+let quiz1 = "Hedwig Eva Maria Kiesler, conocida en Hollywood como Hedy Lamarr, ¿fue la inventora del wifi?";
+let quiz2 = "¿Jean E. Sammet​ desarrolló el lenguaje de programación FORMAC en 1962?";
+let quiz3 = "Mary Kenneth Keller, la primera mujer doctorada en informática en Estados Unidos ¿era monja?";
+let quiz4 = "Radia Joy Perlman trabajando para Intel, ¿consiguió más de 47 patentes?";
+let quiz5 = "Para entrar en Factoría F5, ¿tienes que tener un título de Miss Universo?";
+let quiz6 = "¿Fue Evelyn Berezin recordada como la madre de los procesadores de texto?";
+let quiz7 = "Frances E. Allen, pionera en automatización de tareas paralelas y optimización de compiladores no recibió nunca un premio";
+let quiz8 = "Ángela Ruiz Robles precursora del libro electrónico ¿nació en roma?";
+
+
+
+
